7c9c8c8017be4a96a565bb5e7cd5e61e70e61730
yogendrautgerkar <yogendrautgerkar@gmail.com>
2019-12-16 22:18:24 +0530

    latest

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+ 
+import Container from './component/Container';
+
+
+function App() {
+  return (
+    <div className="App">
+<Container/> 
+    <div></div>
+    </div>
+  );
+}
+
+export default App;
